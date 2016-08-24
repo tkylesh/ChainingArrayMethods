64fc96143d4bfea1362b43a5af7ad1d6c6977a79
Taylor Shuff <tkylesh@gmail.com>
2016-08-23 20:08:29 -0500

    completed chain for array method chaining exercise

---
+++ chaining.js
@@ -0,0 +1,19 @@
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+//sort
+// var fruit = ["cherry", "pear", "apple", "banana"];
+// console.log(fruit.sort());
+
+// var nums = [7, 8, 99, 5, 2, 9, 88];
+// var sortedNums = nums.sort(
+// 	function(first, second){
+// 		return first-second;
+// 	});
+// console.log(sortedNums);
+
+var chain = integers.sort(function(first,second)
+	{return second-first;}).filter(function(int) { return int <= 19; })
+	.reduce(function(previousValue,currentValue, array){
+	return (previousValue*1.5)+(currentValue*1.5)});
+
+console.log(Math.floor(chain));
+
